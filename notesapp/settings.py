e78bb54d7b5025656b91ae43032fc02bd9402d86
sourav2128 <108931336+sourav2128@users.noreply.github.com>
2025-03-16 12:58:31 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'HOST': os.getenv("DB_HOST",'localhost'),
+        'PORT': os.getenv("DB_PORT",'3306'),
     }
 }
 
